b143c435c0871fc12e583931a20516999c28b1c6
bini25 <58245174+bini25@users.noreply.github.com>
2020-05-14 00:47:12 -0400

    Updates

---
+++ precache-manifest.4273d50be5b0874d68a065e8e896887d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af34ca335f04dbc396f6c98422877045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dbe590243c0178c7159",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "687398f0cf096f75c1e9",
+    "url": "/static/js/2.f86ca26e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f86ca26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbe590243c0178c7159",
+    "url": "/static/js/main.ec4ccadf.chunk.js"
+  },
+  {
+    "revision": "c5efb4cb042175ae980f",
+    "url": "/static/js/runtime-main.9063134a.js"
+  }
+]);
